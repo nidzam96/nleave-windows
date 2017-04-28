8da5e90e4b413c1643f61ce7a4a3e8b6c0885f4f
Nidzam <nidzamhussain96@gmail.com>
2017-04-28 17:15:48 +0800

    update

--- resources/views/admin/benefits.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('admin.layout')
-
-@section('pagetitle','Benefits')
-
-@section('section')
-    <div class="col-md-8 col-md-offset-2">
-        <div id="calendar"></div>
-    </div>
-@endsection
